fdacbb5056f4343f3babb61f63458a30e830ba85
JoseFernandez-Pepe <124707301+JoseFernandez-Pepe@users.noreply.github.com>
2023-03-01 18:59:15 -0500

    Create FindTheOddInt.java

---
+++ FindTheOddInt.java
@@ -0,0 +1,15 @@
+public class FindOdd {
+	public static int findIt(int[] a) {
+    int count = 0;
+    for(int i = 0; i < a.length; i++){
+      for(int j = 0; j < a.length; j++){
+        if(a[j] == a[i]) {
+          count++;
+        }
+      }
+      if(count % 2 == 1) return a[i];
+      count = 0;
+    }
+    return 0;
+  }
+}
